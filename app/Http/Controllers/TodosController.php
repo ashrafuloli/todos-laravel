ae113da9d2ec2c133589c5bac5e14d9d013ab7d2
ashrafuloli <rock.ashraful@gmail.com>
2020-03-16 06:04:59 +0600

    clean route

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -8,17 +8,12 @@ class TodosController extends Controller
 {
     public function index()
     {
-        /**
-         * fetch all todos from database
-         * display them in the todos. index page
-         */
-
         return view('todos.index')->with('todos', Todo::all());
     }
 
-    public function show($todoId)
+    public function show(Todo $todo)
     {
-        return view('todos.show')->with('todo', Todo::find($todoId));
+        return view('todos.show')->with('todo', $todo);
     }
 
     public function create()
@@ -44,14 +39,12 @@ class TodosController extends Controller
         return redirect('\todos');
     }
 
-    public function edit($todoId)
+    public function edit(Todo $todo)
     {
-        $todo = Todo::find($todoId);
-
         return view('todos.edit')->with('todo', $todo);
     }
 
-    public function update($todoId)
+    public function update(Todo $todo)
     {
         $this->validate(request(), [
             'name' => 'required|min:5|max:25',
@@ -59,8 +52,6 @@ class TodosController extends Controller
         ]);
 
         $data = request()->all();
-
-        $todo = Todo::find($todoId);
         $todo->name = $data['name'];
         $todo->description = $data['description'];
 
@@ -70,11 +61,9 @@ class TodosController extends Controller
 
     }
 
-    public function destroy($todoId)
+    public function destroy(Todo $todo)
     {
-        $todo = Todo::find($todoId);
         $todo->delete();
-
         return redirect('/todos');
     }
 }
